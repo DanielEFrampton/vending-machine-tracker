2a5c31c5a17775ec138f346fd5941cbf90c19213
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-19 10:04:55 -0700

    Add code to pass user story 2

--- spec/features/machines/user_sees_machine_snacks_on_show_spec.rb
+++ spec/features/machines/user_sees_machine_snacks_on_show_spec.rb
@@ -4,10 +4,14 @@ RSpec.describe 'As a visitor', type: :feature do
   before(:each) do
     @owner = Owner.create(name: 'Daniel Frampton')
     @machine = @owner.machines.create(location: 'Turing Mess Hall')
-    @snack_1 = @machine.snacks.create(name: 'Danritos', price: 3)
-    @snack_2 = @machine.snacks.create(name: 'ChocoDans', price: 2)
-    @snack_3 = @machine.snacks.create(name: 'DanBar', price: 5)
-    @snack_4 = @machine.snacks.create(name: 'D&Ds Candies', price: 8)
+    @snack_1 = Snack.create(name: 'Danritos', price: 300)
+    @snack_2 = Snack.create(name: 'ChocoDans', price: 250)
+    @snack_3 = Snack.create(name: 'DanBar', price: 75)
+    @snack_4 = Snack.create(name: 'D&Ds Candies', price: 175)
+    MachineSnack.create(machine: @machine, snack: @snack_1, quantity: 1)
+    MachineSnack.create(machine: @machine, snack: @snack_2, quantity: 1)
+    MachineSnack.create(machine: @machine, snack: @snack_3, quantity: 1)
+    MachineSnack.create(machine: @machine, snack: @snack_4, quantity: 1)
   end
 
   describe 'When I visit a vending machine show page' do
@@ -15,10 +19,10 @@ RSpec.describe 'As a visitor', type: :feature do
       visit "/machines/#{@machine.id}"
 
       expect(page).to have_content("Snacks in This Machine:")
-      expect(page).to have_content("Snack: #{@snack_1.name} | Price: #{@snack_1.price}")
-      expect(page).to have_content("Snack: #{@snack_2.name} | Price: #{@snack_2.price}")
-      expect(page).to have_content("Snack: #{@snack_3.name} | Price: #{@snack_3.price}")
-      expect(page).to have_content("Snack: #{@snack_4.name} | Price: #{@snack_4.price}")
+      expect(page).to have_content("Snack: #{@snack_1.name} | Price: $3.00")
+      expect(page).to have_content("Snack: #{@snack_2.name} | Price: $2.50")
+      expect(page).to have_content("Snack: #{@snack_3.name} | Price: $0.75")
+      expect(page).to have_content("Snack: #{@snack_4.name} | Price: $1.75")
     end
   end
 end
